62bbe7095678a6f2b4cc98936743dbbf66dd5275
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-16 20:54:17 -0400

    user update

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,16 +1,22 @@
 package Model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Jacksonized
 @Document(collection = "users")
 public class User {
+    @Id
     private String id;
     private String name;
     private int age;
